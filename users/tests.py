583c62ec87fe7287db164c463179ed0d5b7deaf3
Peizhi <parisa1206@gmail.com>
2019-09-25 14:10:57 -0700

    add customuser

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .forms import CustomUserCreationForm
 
 # Create your tests here.
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="will", email="will@gmail.com", password="willspassword"
+        )
+        self.assertEqual(user.username, "will")
+        self.assertEqual(user.email, "will@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username="parisa", email="parisa@gmail.com", password="parisaspassword"
+        )
+        self.assertEqual(user.username, "parisa")
+        self.assertEqual(user.email, "parisa@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
